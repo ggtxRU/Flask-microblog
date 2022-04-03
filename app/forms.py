571218ad45ff99e374d3259825f651dbd8c82585
Kirill Kulakov <gularovkirill@gmail.com>
2022-04-04 00:43:33 +0300

    v0.2

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 from app.models import User
 
 
 class LoginForm(FlaskForm):
+    """Форма входа в систему"""
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
@@ -12,6 +13,7 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
+    """Форма регистрации клиента"""
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -20,11 +22,31 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Register')
 
     def validate_username(self, username):
+        """Проверяем, что имя пользователя, введенное в форму, не существует в базе данных"""
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Это имя пользователя уже занято')
 
     def validate_email(self, email):
+        """Проверяем, что почтовый ящик, введенный в форму, не существует в базе данных"""
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Этот почтовый ящик уже используется')
+
+class EditProfileForm(FlaskForm):
+    """Форма изменения данных на странице"""
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        """Получаем изначальный username пользователя, до попытки изменить его, в переменную original_username"""
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self,username):
+        "Сравниваем, чтобы измененный и оригинальный username не совпадали, далее проверяем чтобы этот username не был занят другим пользователем"
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username!')
--- app/models.py
+++ app/models.py
@@ -1,9 +1,16 @@
+from email.policy import default
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
+"""Добавление вспомогательной таблицы подписчиков/подписок в базу данных"""
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +18,13 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin = (followers.c.follower_id == id),
+        backref = db.backref('followers', lazy='dynamic'),lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -21,6 +35,10 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
